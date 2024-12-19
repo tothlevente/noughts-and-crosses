59261d07c2a05134db6ebb52f6f2a9caeec8c555
Levente <168279599+tothlevente@users.noreply.github.com>
2024-12-19 10:51:07 +0000

    Develop footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import Footer from "./components/contents/Footer";
 import Header from "./components/contents/Header";
 
 import { ThemeProvider } from "./components/theme-provider";
@@ -8,7 +9,11 @@ export default function App() {
       defaultTheme="light"
       storageKey="vite-ui-theme"
     >
-      <Header />
+      <div className="site-wrapper">
+        <Header />
+        <div className="content"></div>
+        <Footer />
+      </div>
     </ThemeProvider>
   );
 }
